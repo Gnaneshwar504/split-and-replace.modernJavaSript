b06d52287accfadf5d439189806f03abcff41463
Gnaneshwar <86634672+Gnaneshwar504@users.noreply.github.com>
2022-01-16 17:50:47 +0530

    Create split.js

---
+++ split.js
@@ -0,0 +1,44 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString.trim().split("\n").map((str) => str.trim());
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/* Please do not modify anything above this line */
+
+function main() {
+  const inputString = readLine();
+  const separator = readLine();
+  const replaceString = readLine();
+  
+let array = inputString.split(separator);
+
+
+//let seperator(inputString, separatorString, replaceString);
+
+function seperator(inputString, separator, replaceString ) {
+    let arr = inputString.split(separator);
+    let res = arr.map(item => item.length > 7 ? item = replaceString : item = item);
+
+   
+    
+    res.join("");
+    console.log(...res);
+}
+seperator(inputString,separator,replaceString);
+}
